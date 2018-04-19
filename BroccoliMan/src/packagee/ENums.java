23e12431108e5fff285a90ab9da007aca147b217
sttsalei <TAleithe@hotmail.de>
2018-04-19 09:28:41 +0200

    Implemented int[] enumarators to merge two arrays, switching between elements in them. Implemented int[] arrayLengthEvolve to fit two arrays of different size

---
+++ BroccoliMan/src/packagee/ENums.java
@@ -0,0 +1,33 @@
+package packagee;
+
+public class ENums {
+    public static int[] enumarators(int[] arr1, int[] arr2){
+        int[] resArr = new int[arr1.length+arr2.length];
+        int flag, i, j;
+        i = j = flag = 0;
+        if(arr1.length != arr2.length){
+            if(arr1.length > arr2.length) arr2 = arrayLengthEvolve(arr2, arr1.length);
+            else arr1 = arrayLengthEvolve(arr1, arr2.length);
+        }
+        while((j+i) < arr1.length+arr2.length) {
+            if (flag == 0) {
+                resArr[i+j] = arr1[i];
+                flag = 1;
+                i++;
+            }else{
+                resArr[i+j] = arr2[j];
+                flag = 0;
+                j++;
+            }
+        }
+        return resArr;
+    }
+    private static int[] arrayLengthEvolve(int[] arr, int length){
+        int[] newArr = new int[length];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0)newArr[i] = arr[i];
+            else newArr[i] = 0;
+        }
+        return newArr;
+    }
+}
--- BroccoliMan/src/packagee/Main.java
+++ BroccoliMan/src/packagee/Main.java
@@ -2,6 +2,13 @@ package packagee;
 
 public class Main {
     public static void main(String[] args){
+        int[] arr = new int[0];
         System.out.println("Test");
+        arr = ENums.enumarators(new int[] {1,2,3}, new int[] {4,5,6});
+        for (int i:arr) {
+            System.out.print(i+" ");
+        }
+
     }
+
 }
